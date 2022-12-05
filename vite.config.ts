8e9afa99a0d3aab38306499a3ed8d08096bf00a9
Dasha <lady.renfri@gmail.com>
2022-12-05 19:32:27 +0300

    geoboard-frontend

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    }),
+  ],
+
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src'),
+      },
+    ],
+  },
+})
