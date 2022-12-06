58d6c5d373f99c307fd82337b6228731871d9924
Dasha <lady.renfri@gmail.com>
2022-12-06 21:28:07 +0300

    Test pre-commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const convertStringToBool = (bla: string): boolean => {
 }
 
 function App() {
-  const one = 2
+  const one = 3
   const [count, setCount] = useState(0)
   testFunc(15)
   convertStringToBool('15')
